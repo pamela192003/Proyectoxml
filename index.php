9689b01cdb3eac112824747a18e2f0001381c7e8
pamela rojas <rojasataupillcopamels@gmail.com>
2025-11-27 09:21:35 -0500

    🍎

---
+++ index.php
@@ -0,0 +1,176 @@
+<programas_estudio>
+    <disenio_programacion_web>
+        <nombre>DISEÑO Y PROGRAMACÍON WEB</nombre>
+        <modulos>
+            <modulo1>
+                <nombre> ANÁLISIS Y DISEÑO DE SISTEMAS WEB</nombre>
+                <periodos>
+                    <p1>
+                        <nombre>I</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>ANÁLISIS Y DISEÑO DE SISTEMAS</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>FUNDAMENTOS DE PROGRAMACIÓN</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>REDES E INTERNET</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>INTRODUCCIÓN DE BASE DE DATOS</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>ARQUITECTURA DE COMPUTADORAS</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>COMUNICACIÓN ORAL</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>APLICACIONES EN INTERNET</nombre>
+                            </ud7>
+                        </unidades_didacticas>
+                    </p1>
+                    <p2>
+                        <nombre>II</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>OFIMÁTICA</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>INTERPRETACIÓN Y PRODUCCIÓN DE TEXTOS</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>METODOLOGÍA DE DESARROLLO DE SOFTWARE</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>PROGRAMACIÓN ORIENTADA A OBJETOS</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>AEQUITECTURA DE SERVIDORES WEB</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>APLICACIONES SISTEMATIZADAS</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>TALLER DE BASE DE DATOS</nombre>
+                            </ud7>
+                        </unidades_didacticas>
+                    </p2>
+                </periodos>
+            </modulo1>
+            <modulo2>
+                <nombre>DESARROLLO DE APLICACIONES WEB</nombre>
+                <periodos>
+                    <p1>
+                        <nombre>III</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>ADMINISTRACIÓN DE BASE DE DATOS</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>PROGRAMACIÓN DE APLICACIONES WEB</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>DISEÑO DE INTERFACES WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>PRUEBAS DE SOFTWARE</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>INGLÉS PARA LA COMUNICACIÓN ORAL</nombre>
+                            </ud5>
+                        </unidades_didacticas>
+                    </p1>
+                    <p2>
+                        <nombre>IV</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>DESARROLLO DE ENTORNOS WEB</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>PROGRAMACIÓN DE SOLUCIONES WEB</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>PROYECTOS DE SOFTWARE</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>SEGURIDAD EN APLICACIONES WEB</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>COMPRENSIÓN Y REDACCIÓN EN INGLÉS</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>COMPORTAMIENTO ÉTICO</nombre>
+                            </ud6>
+                        </unidades_didacticas>
+                    </p2>
+                </periodos>
+            </modulo2>
+            <modulo3>
+                <nombre>DISEÑO DE SERVICIOS WEB</nombre>
+                <periodos>
+                    <p1>
+                        <nombre>V</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>PROGRAMACIÓN DE APLICACIONES MÓVILES</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>MARKETING DIGITAL</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>DISEÑO DE SOLUCIONES WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>GESTIÓN Y ADMINISTRACIÓN DE SITIOS WEB</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>DIAGRAMACIÓN DIGITAL</nombre>
+                            </ud5>
+                            <ud6>
+                                <nombre>SOLUCIÓN DE PROBLEMAS</nombre>
+                            </ud6>
+                            <ud7>
+                                <nombre>OPORTUNIDADES DE NEGOCIOS</nombre>
+                            </ud7>
+                        </unidades_didacticas>
+                    </p1>
+                    <p2>
+                        <nombre>VI</nombre>
+                        <unidades_didacticas>
+                            <ud1>
+                                <nombre>PLATAFORMA DE SERVICIOS WEB</nombre>
+                            </ud1>
+                            <ud2>
+                                <nombre>ILUSTRACIÓN Y GRÁFICA DIGITAL</nombre>
+                            </ud2>
+                            <ud3>
+                                <nombre>ADMINISTRACIÓN DE SERVIDORES WEB</nombre>
+                            </ud3>
+                            <ud4>
+                                <nombre>COMERCIO ELECTRÓNICO</nombre>
+                            </ud4>
+                            <ud5>
+                                <nombre>PLAN DE NEGOCIOS</nombre>
+                            </ud5>
+                        </unidades_didacticas>
+                    </p2>
+                </periodos>
+            </modulo3>
+
+        </modulos>
+    </disenio_programacion_web>
+    <enfermeria_tecnica>
+
+    </enfermeria_tecnica>
+    <industrias_alimentos_bebidas>
+
+    </industrias_alimentos_bebidas>
+    <mecatronica_automotriz>
+
+    </mecatronica_automotriz>
+    <produccion_agropecuaria>
+
+    </produccion_agropecuaria>
+</programas_estudio>
